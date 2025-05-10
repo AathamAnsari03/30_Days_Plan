b44f6e18b14a9308a3f2877193e740e99ceebb99
AathamAnsari03 <ansariseenu84@gmail.com>
2025-05-10 20:21:36 +0530

    Update Backend

--- Flask/Day_7/app.py
+++ Flask/Day_7/app.py
@@ -2,8 +2,12 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token
+from flask_cors import CORS
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['JWT_SECRET_KEY'] = 'code-red'
 jwt = JWTManager(app)
@@ -29,7 +33,7 @@ def signup():
     new_user = User(name=name, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({"message": "User created successfully"}), 201
+    return jsonify({"message": "User created successfully"})
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -39,10 +43,18 @@ def login():
 
     user = User.query.filter_by(name=name).first()
     if user and check_password_hash(user.password, password):
-        access_token = create_access_token(identity={'id' : user.id, 'name': user.name})
-        return jsonify(access_token = access_token), 200
+        access_token = create_access_token(identity=str(user.id))
+        return jsonify({"message" : "Login successful", "access_token": access_token})
     else:
-        return jsonify({"message": "Invalid credentials"}), 401
+        return jsonify({"message": "Invalid credentials"})
+    
+@app.route('/dashboard', methods=['GET'])
+@jwt_required()
+def dashboard():
+    user_id = get_jwt_identity()
+    user = User.query.get(int(user_id))
+    return jsonify({"message": f"Welcome {user.name} to your dashboard"})
+
 
 
 if __name__ == '__main__':
